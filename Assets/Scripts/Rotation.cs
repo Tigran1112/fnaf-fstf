101bccc8e7346c44e1df6f67df45b67c51d0e9c4
Tigran <tarasdarovtigran@gmail.com>
2025-08-14 11:26:21 +0300

    fierst

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public float XRot;
+    public float sensitivity;
+    public float min, max;
+
+    public GameObject FL, FF, FR;
+
+    void Update()
+    {
+        XRot += Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        XRot = Mathf.Clamp(XRot, min, max);
+
+        var angles = transform.eulerAngles;
+        angles.y = XRot;
+        transform.eulerAngles = angles;
+
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            if (XRot > 45f) OnFlashlight(FL);
+            else if (45f < XRot && XRot < 135f) OnFlashlight(FF);
+            else if (135f < XRot) OnFlashlight(FR);
+        }
+        else OnFlashlight(null);
+    }
+    void OnFlashlight(GameObject flashlight)
+    {
+        FL.SetActive(false);
+        FF.SetActive(false);
+        FR.SetActive(false);
+
+        if(flashlight != null) flashlight.SetActive(true);
+    }
+}
