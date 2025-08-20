e0c38382faa3b6b082474181383e36cf17fcde4a
Tigran <tarasdarovtigran@gmail.com>
2025-08-20 21:41:26 +0300

    Revert "aaaa"
    This reverts commit a0aeaf54398f9302a9dec34e5a6e0e581378f060.

--- Assets/Scripts/n1/Animatronics/Bonnie.cs
+++ Assets/Scripts/n1/Animatronics/Bonnie.cs
@@ -1,43 +1,110 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Bonnie : N1Base
+public class Bonnie : MonoBehaviour
 {
+    public GameObject[] pos;
+    private int index;
+
+    [Range(0, 20)]
+    public int AI;
+    private float _minDelay, _maxDelay, _chance;
+
     public GameObject[] flashlight;
+
+    public GameObject trans;
+    public Animator run, jumpscare;
+
+    private bool isCheck;
+    private bool isAttacking;
+
+    public GameObject tab;
+    public Cameras cameras;
+
     private float timeToRun;
 
-    protected void Update()
+    void Awake()
     {
-        if (!isAttacking)
+        foreach (var c in pos) c.SetActive(false);
+        if (AI == 0) return;
+
+        float t = (AI - 1f) / 19f;
+        float s = Mathf.SmoothStep(15f, 1f, t);
+        _minDelay = Mathf.Lerp(15f, 1f, s);
+        _maxDelay = Mathf.Lerp(25f, 7f, s);
+        _chance = Mathf.RoundToInt(Mathf.Lerp(30f, 100f, s));
+
+        index = pos.Length - 1;
+        pos[index].SetActive(true);
+
+        InvokeRepeating("Walk", Random.Range(_minDelay, _maxDelay), Random.Range(_minDelay, _maxDelay));
+    }
+
+    IEnumerator TransEffect()
+    {
+        trans.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        trans.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (isCheck && !isAttacking)
         {
-            CheckFlashlight();
+            timeToRun += Time.deltaTime;
+
+            if (timeToRun >= 5f)
+            {
+                foreach (var f in flashlight)
+                {
+                    if (f.activeSelf)
+                    {
+                        isAttacking = true;
+                        StartCoroutine(Attack());
+                        break;
+                    }
+                }
+                run.SetTrigger("Out");
+                Invoke("GoBack", 2f);
+            }
         }
     }
 
-    protected void CheckFlashlight()
+    void Walk()
     {
-        timeToRun += Time.deltaTime;
-
-        if (timeToRun >= 5f)
+        if (Random.Range(0, 101) <= _chance && index != 0)
         {
-            foreach (var f in flashlight)
-            {
-                if (f.activeSelf)
-                {
-                    isAttacking = true;
-                    StartCoroutine(Attack());
-                    timeToRun = 0f;
-                    return;
-                }
-                else
-                {
-                    isAttacking = false;
-                    base.GoBack();
-                    timeToRun = 0f;
-                }
-            }
+            StartCoroutine(TransEffect());
+            pos[index].SetActive(false);
+            index--;
+            pos[index].SetActive(true);
+        }
+
+        if (index == 0 && !isCheck)
+        {
+            isCheck = true;
             timeToRun = 0f;
         }
     }
 
+    IEnumerator Attack()
+    {
+        run.SetTrigger("Run");
+        yield return new WaitForSeconds(2f);
+
+        cameras.Close();
+        Destroy(tab);
+
+        jumpscare.SetTrigger("Scream");
+        yield return new WaitForSeconds(1.5f);
+
+        SceneManager.LoadScene("Dead");
+    }
+    void GoBack()
+    {
+        pos[index].SetActive(false);
+        index = pos.Length - 1;
+        pos[index].SetActive(true);
+    }
 }
--- Assets/Scripts/n1/Animatronics/Freddy.cs
+++ Assets/Scripts/n1/Animatronics/Freddy.cs
@@ -1,9 +1,52 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Freddy : N1Base
+public class Freddy : MonoBehaviour
 {
-    protected void Update()
+    public GameObject[] pos;
+    private int index;
+
+    [Range(0, 20)]
+    public int AI;
+    private float _minDelay, _maxDelay, _chance;
+
+    public GameObject Camera;
+
+    public GameObject trans;
+    public Animator run, jumpscare;
+
+    private bool isAttacking;
+    private bool returning;
+
+    public GameObject tab;
+    public Cameras cameras;
+
+    void Awake()
+    {
+        foreach (var c in pos) c.SetActive(false);
+        if (AI == 0) return;
+
+        float t = (AI - 1f) / 19f;
+        float s = Mathf.SmoothStep(15f, 1f, t);
+        _minDelay = Mathf.Lerp(15f, 1f, s);
+        _maxDelay = Mathf.Lerp(25f, 7f, s);
+        _chance = Mathf.RoundToInt(Mathf.Lerp(30f, 100f, s));
+
+        index = pos.Length - 1;
+        pos[index].SetActive(true);
+
+        InvokeRepeating("Walk", Random.Range(_minDelay, _maxDelay), Random.Range(_minDelay, _maxDelay));
+    }
+
+    IEnumerator TransEffect()
+    {
+        trans.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        trans.SetActive(false);
+    }
+
+    void Update()
     {
         if (Camera.activeSelf && !returning && index != 0)
         {
@@ -12,7 +55,24 @@ public class Freddy : N1Base
         }
     }
 
-    protected IEnumerator ReturnToStart()
+    void Walk()
+    {
+        if (Random.Range(0, 101) <= _chance && index != 0 && !returning)
+        {
+            StartCoroutine(TransEffect());
+            pos[index].SetActive(false);
+            index--;
+            pos[index].SetActive(true);
+        }
+
+        if (index == 0 && !isAttacking)
+        {
+            isAttacking = true;
+            StartCoroutine(Attack());
+        }
+    }
+
+    IEnumerator ReturnToStart()
     {
         yield return new WaitForSeconds(3f);
         StartCoroutine(TransEffect());
@@ -20,5 +80,22 @@ public class Freddy : N1Base
         index = pos.Length - 1;
         pos[index].SetActive(true);
         returning = false;
+    }
+
+    IEnumerator Attack()
+    {
+        float delay = Random.Range(20f, 60f);
+        yield return new WaitForSeconds(delay);
+
+        run.SetTrigger("Run");
+        yield return new WaitForSeconds(2f);
+
+        cameras.Close();
+        Destroy(tab);
+
+        jumpscare.SetTrigger("Scream");
+        yield return new WaitForSeconds(1.5f);
+
+        SceneManager.LoadScene("Dead");
     }
 }
